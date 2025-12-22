773a728cf74d6ae94487c01a2f777040f4fac07c
marikgryc <markogricuk@gmail.com>
2025-12-22 15:43:59 +0200

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Зараз створимо стилі
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Логотип */}
+        <Link to="/" className="navbar-logo">
+          Leafy <span style={{ color: '#4ade80' }}>.</span>
+        </Link>
+
+        {/* Меню */}
+        <div className="navbar-menu">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/explore" className="nav-link">Explore</Link>
+          <Link to="/profile" className="nav-link">Profile</Link>
+        </div>
+
+        {/* Аватарка (заглушка) */}
+        <Link to="/profile" className="navbar-profile">
+          <div className="avatar-circle">U</div>
+        </Link>
+      </div>
+    </nav>
+  );
+}
