e68af688a868f59e9b88b9aafdc305d476eb6fe0
marikgryc <markogricuk@gmail.com>
2025-12-22 16:02:03 +0200

    styles fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import UserProfile from './pages/UserProfile';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'; 
 import HomePage from './pages/HomePage';
-
-const Navbar = () => (
-  <nav style={{ padding: 20, background: '#000', borderBottom: '1px solid #333' }}>
-    <Link to="/" style={{ color: 'white', marginRight: 20 }}>Home</Link>
-    <Link to="/profile" style={{ color: 'white' }}>Profile</Link>
-  </nav>
-);
+// import UserProfile from './pages/UserProfile'; 
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar /> {}
+      
       <Routes>
-      <Route path="/" element={<HomePage />} />
-      {/* <Route path="/profile" element={<UserProfile />} /> */}
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/profile" element={<UserProfile />} /> */}
       </Routes>
     </BrowserRouter>
-  );  
+  );
 }
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Зараз створимо стилі
+import './Navbar.css'; 
 
 export default function Navbar() {
   return (
